2102745f84649857867480f8626ecc1b45dc1569
Eva Liu <evaliu2046@gmail.com>
2021-08-03 09:26:47 +0800

    Add file for lambda apigateway implementation

---
+++ aws-lambda-apigateway/src/hello.js
@@ -0,0 +1,19 @@
+//Lambda function code
+
+module.exports.handler = async (event) => {
+  console.log('Event', event);
+  let responseMessage = 'Hello, World!';
+
+  if (event.queryStringParameters && event.queryStringPatameters['Name']) {
+    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({message: responseMessage,}),
+  }
+}
+
---
+++ aws-lambda-apigateway/variables.tf
@@ -0,0 +1,7 @@
+#Input Variable definitions
+
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-west-2"
+}
